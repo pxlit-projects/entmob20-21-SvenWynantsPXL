64afa3a59bd6ae69a026aab78a563bb226734819
Sven Wynants <sven.wynants@hotmail.com>
2020-11-18 14:22:32 +0100

    initialization frontend

---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmartHouseLights.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmartHouseLights.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
