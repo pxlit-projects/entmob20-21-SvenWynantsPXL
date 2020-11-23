21cb0d080d4e64b2f45fb09f0061ff22db050457
Sven Wynants <sven.wynants@hotmail.com>
2020-11-23 17:25:38 +0100

    LightDaoImpl created

---
+++ project/smarthome/src/main/java/be/pxl/smarthome/dao/impl/LightDaoImpl.java
@@ -0,0 +1,13 @@
+package be.pxl.smarthome.dao.impl;
+
+import be.pxl.smarthome.dao.LightDao;
+import be.pxl.smarthome.models.Light;
+
+import java.util.List;
+
+public class LightDaoImpl implements LightDao {
+    @Override
+    public List<Light> getAllLights() {
+        return null;
+    }
+}
