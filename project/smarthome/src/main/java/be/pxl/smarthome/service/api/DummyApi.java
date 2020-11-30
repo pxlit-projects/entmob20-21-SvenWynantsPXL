1f72a626d9a8c453fdc67ae938c42bd193993c4e
Sven Wynants <sven.wynants@hotmail.com>
2020-11-30 23:31:30 +0100

    seeded 2 lights in dummy api

--- project/smarthome/src/main/java/be/pxl/smarthome/service/api/DummyApi.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/api/DummyApi.java
@@ -1,4 +1,21 @@
 package be.pxl.smarthome.service.api;
 
+import be.pxl.smarthome.models.Light;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DummyApi {
+    private List<Light> lights;
+    public DummyApi() {
+        lights = new ArrayList<>();
+        Light light1 = new Light(100, false, "dummy light", "Dummy light 1", "Dummy");
+        Light light2 = new Light(100, false, "dummy light", "Dummy light 2", "Dummy");
+        lights.add(light1);
+        lights.add(light2);
+    }
+
+    public List<Light> getAllLights(){
+        return lights;
+    }
 }
