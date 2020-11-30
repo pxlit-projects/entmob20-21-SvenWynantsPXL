119a7a6a9621c83621e6a188b77dca38dc3b2003
Sven Wynants <sven.wynants@hotmail.com>
2020-11-30 23:31:51 +0100

    added api's to get all the lights from different api's

--- project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
@@ -2,15 +2,26 @@ package be.pxl.smarthome.service.impl;
 
 import be.pxl.smarthome.models.Light;
 import be.pxl.smarthome.service.LightApiService;
-import org.springframework.stereotype.Service;
+import be.pxl.smarthome.service.api.DummyApi;
+import be.pxl.smarthome.service.api.IkeaApi;
+import be.pxl.smarthome.service.api.PhilipsApi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LightApiServiceImpl implements LightApiService {
+    private DummyApi dummyApi;
+    private IkeaApi ikeaApi;
+    private PhilipsApi philipsApi;
 
     @Override
     public List<Light> getAllLightsInNetwork() {
-        return new ArrayList<Light>();
+        List<Light> allLights = new ArrayList<>();
+        allLights = dummyApi.getAllLights();
+        allLights = Stream.concat(allLights.stream(), ikeaApi.getAllLights().stream()).collect(Collectors.toList());
+        
+        return allLights;
     }
 }
