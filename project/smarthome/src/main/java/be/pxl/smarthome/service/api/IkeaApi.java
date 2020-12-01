70cb29dbc9377ce231f7d7224c95c18c004eb262
Sven Wynants <sven.wynants@hotmail.com>
2020-12-01 19:47:03 +0100

    httpclient added

--- project/smarthome/src/main/java/be/pxl/smarthome/service/api/IkeaApi.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/api/IkeaApi.java
@@ -2,14 +2,26 @@ package be.pxl.smarthome.service.api;
 
 import be.pxl.smarthome.models.Light;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 public class IkeaApi {
+
+    private HttpClient client;
+
     public IkeaApi(){
+        client = HttpClient.newHttpClient();
     }
 
     public List<Light> getAllLights(){
+        /*var request = HttpRequest.newBuilder(
+                URI.create("https://ikeabridge/api/<username>/lights"))
+                .build();
+
+         */
         return new ArrayList<>();
     }
 }
