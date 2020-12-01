a3471ba086e9d0ff6ed3b3bc466b6f6ed53256e0
Sven Wynants <sven.wynants@hotmail.com>
2020-12-02 00:33:48 +0100

    a light thats added with my backend will be added to the api as well

--- project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightServiceImpl.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightServiceImpl.java
@@ -3,15 +3,21 @@ package be.pxl.smarthome.service.impl;
 import be.pxl.smarthome.dao.LightDao;
 import be.pxl.smarthome.dto.LightDto;
 import be.pxl.smarthome.models.Light;
+import be.pxl.smarthome.service.LightApiService;
 import be.pxl.smarthome.service.LightService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class LightServiceImpl implements LightService {
 
     @Autowired
     private LightDao dao;
+    @Autowired
+    private LightApiService lightApiService;
 
     public Light addLight(LightDto lightDto) {
         Light light = new Light();
@@ -22,10 +28,12 @@ public class LightServiceImpl implements LightService {
         light.setType(lightDto.type);
 
         dao.save(light);
+        lightApiService.addLight(light);
 
         return light;
     }
 
+    //To import the lights from the bridges and dummy to this database
     @Override
     public Light addLight(Light light) {
         dao.save(light);
