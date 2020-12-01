b13e46a589455f210fe6afaa0bef333628bed35c
Sven Wynants <sven.wynants@hotmail.com>
2020-12-02 00:33:09 +0100

    autowired the api interfaces

--- project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
@@ -2,26 +2,48 @@ package be.pxl.smarthome.service.impl;
 
 import be.pxl.smarthome.models.Light;
 import be.pxl.smarthome.service.LightApiService;
-import be.pxl.smarthome.service.api.DummyApi;
-import be.pxl.smarthome.service.api.IkeaApi;
-import be.pxl.smarthome.service.api.PhilipsApi;
-
+import be.pxl.smarthome.service.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Service
 public class LightApiServiceImpl implements LightApiService {
-    private DummyApi dummyApi = new DummyApi();
-    private IkeaApi ikeaApi = new IkeaApi();
-    private PhilipsApi philipsApi = new PhilipsApi();
+
+    private LightApi lightApi;
+    @Autowired
+    private DummyApi dummyApi;
+    @Autowired
+    private IkeaApi ikeaApi;
+    @Autowired
+    private PhilipsApi philipsApi;
 
     @Override
     public List<Light> getAllLightsInNetwork() {
         List<Light> allLights = new ArrayList<>();
         allLights = dummyApi.getAllLights();
         allLights = Stream.concat(allLights.stream(), ikeaApi.getAllLights().stream()).collect(Collectors.toList());
-        allLights = Stream.concat(allLights.stream(),philipsApi.getAllLights().stream()).collect(Collectors.toList());
+        allLights = Stream.concat(allLights.stream(), philipsApi.getAllLights().stream()).collect(Collectors.toList());
         return allLights;
+    }
+
+    @Override
+    public void addLight(Light light) {
+        lightApi = checkManufacturer(light.getManufacturerName());
+
+        lightApi.addLight(light);
+    }
+
+    private LightApi checkManufacturer(String name) {
+        if (name.toLowerCase().equals("ikea")) {
+            return ikeaApi;
+        } else if (name.toLowerCase().equals("philips")){
+            return philipsApi;
+        } else {
+            return dummyApi;
+        }
     }
 }
