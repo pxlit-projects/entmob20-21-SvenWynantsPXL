37de906178ded8adc067cce4c7a32285fea9ec42
Sven Wynants <sven.wynants@hotmail.com>
2020-12-01 19:47:19 +0100

    request to get all existing lights in network

--- project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/service/impl/LightApiServiceImpl.java
@@ -12,16 +12,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LightApiServiceImpl implements LightApiService {
-    private DummyApi dummyApi;
-    private IkeaApi ikeaApi;
-    private PhilipsApi philipsApi;
+    private DummyApi dummyApi = new DummyApi();
+    private IkeaApi ikeaApi = new IkeaApi();
+    private PhilipsApi philipsApi = new PhilipsApi();
 
     @Override
     public List<Light> getAllLightsInNetwork() {
         List<Light> allLights = new ArrayList<>();
         allLights = dummyApi.getAllLights();
         allLights = Stream.concat(allLights.stream(), ikeaApi.getAllLights().stream()).collect(Collectors.toList());
-        
+        allLights = Stream.concat(allLights.stream(),philipsApi.getAllLights().stream()).collect(Collectors.toList());
         return allLights;
     }
 }
