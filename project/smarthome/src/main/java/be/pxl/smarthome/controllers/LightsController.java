e2b66bc290d986efdba6a20d5960263335ce0044
Sven Wynants <sven.wynants@hotmail.com>
2020-12-02 14:58:38 +0100

    removed unused apiService

--- project/smarthome/src/main/java/be/pxl/smarthome/controllers/LightsController.java
+++ project/smarthome/src/main/java/be/pxl/smarthome/controllers/LightsController.java
@@ -2,13 +2,13 @@ package be.pxl.smarthome.controllers;
 
 import be.pxl.smarthome.dto.LightDto;
 import be.pxl.smarthome.models.Light;
+import be.pxl.smarthome.service.LightApiService;
 import be.pxl.smarthome.service.LightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
