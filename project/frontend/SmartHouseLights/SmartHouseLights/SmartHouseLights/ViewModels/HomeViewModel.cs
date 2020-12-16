4ab250ef61c3d2cd46a0eedb619a2fcc2eef8f0d
Sven Wynants <sven.wynants@hotmail.com>
2020-12-16 12:26:18 +0100

    HomePage inherits from ViewModelBase

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/HomeViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/HomeViewModel.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace SmartHouseLights.ViewModels
 {
-    public class HomeViewModel : BaseViewModel
+    public class HomeViewModel : ViewModelBase
     {
         public User User { get; set; }
         public HomeViewModel()
--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/ViewModelBase.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/ViewModelBase.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using SmartHouseLights.Annotations;
 
@@ -7,6 +9,27 @@ namespace SmartHouseLights.ViewModels
     public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        string title = string.Empty;
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
