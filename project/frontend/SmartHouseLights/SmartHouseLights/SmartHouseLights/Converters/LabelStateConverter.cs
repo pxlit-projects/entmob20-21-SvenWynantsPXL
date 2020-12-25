25c6b7111bb1a4b9b6e850bd2539d29cc761efea
Sven <sven.wynants@hotmail.com>
2020-12-25 16:24:09 +0100

    design of lightdetailpage slightly changed

---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Converters/LabelStateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SmartHouseLights.Converters
+{
+    public class LabelStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool onState = (bool) value;
+            if (onState)
+            {
+                return "On";
+            }
+
+            return "Off";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
