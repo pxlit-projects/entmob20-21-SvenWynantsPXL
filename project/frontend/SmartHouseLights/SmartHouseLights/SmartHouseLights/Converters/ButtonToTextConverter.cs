d293688cffc482dc85c2d828136f12b32e009ec7
Sven <sven.wynants@hotmail.com>
2020-12-25 16:24:23 +0100

    button text adjusted with converter

---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Converters/ButtonToTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SmartHouseLights.Converters
+{
+    public class ButtonToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool state = (bool)value;
+
+            if (state)
+            {
+                return "Turn light off";
+            }
+
+            return "Turn light on";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
