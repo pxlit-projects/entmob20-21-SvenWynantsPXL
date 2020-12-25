64fe4ace80708c065a99b9a3d2e28de6bf2620b3
Sven <sven.wynants@hotmail.com>
2020-12-25 16:23:20 +0100

    created GroupService

---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Services/GroupService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using SmartHouseLights.Models;
+using SmartHouseLights.Services.Interfaces;
+
+namespace SmartHouseLights.Services
+{
+    public class GroupService : IGroupService
+    {
+        public List<LightGroup> GetAllGroups()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LightGroup TurnAllLightsOnInGroup(int groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public LightGroup TurnAllLightsOffInGroup(int groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Services/Interfaces/IGroupService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SmartHouseLights.Models;
+
+namespace SmartHouseLights.Services.Interfaces
+{
+    public interface IGroupService
+    {
+        List<LightGroup> GetAllGroups();
+        LightGroup TurnAllLightsOnInGroup(int groupId);
+        LightGroup TurnAllLightsOffInGroup(int groupId);
+    }
+}
