64784f7bc9f8695d46fbae957070d4937b85fae8
Sven Wynants <sven.wynants@hotmail.com>
2020-12-27 03:55:09 +0100

    functionality for groups added in the viewmodel

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/GroupListViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/GroupListViewModel.cs
@@ -1,7 +1,73 @@
-﻿namespace SmartHouseLights.ViewModels
+﻿using System.Collections.Generic;
+using SmartHouseLights.Models;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Util;
+using Xamarin.Forms;
+
+namespace SmartHouseLights.ViewModels
 {
-    public class GroupListViewModel
+    public class GroupListViewModel : ViewModelBase
     {
-        
+        private readonly INavigationService _navigationService;
+        private readonly IGroupService _groupService;
+
+        private Command _flipSwitchCommand;
+
+        public Command FlipSwitchCommand => _flipSwitchCommand ??
+                                            (_flipSwitchCommand = new Command<int>(OnFlipPressed));
+        public Command GroupSelectedCommand => new Command<int>(OnGroupSelected);
+
+        private List<LightGroup> _groups;
+
+        public List<LightGroup> Groups
+        {
+            get => _groups;
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GroupListViewModel(INavigationService navigationService, IGroupService groupService)
+        {
+            _navigationService = navigationService;
+            _groupService = groupService;
+            Title = "LightGroups";
+            Groups = _groupService.GetAllGroups();
+        }
+
+        private void OnGroupSelected(int id)
+        {
+            //_navigationService.NavigateToAsync(nameof(LightDetailsView));
+            MessagingCenter.Instance.Send(this, MessageConstants.GroupSelected, Groups[id]);
+        }
+
+        private void OnFlipPressed(int id)
+        {
+            if (Groups[GetListId(id)].AllOnState)
+            {
+                _groupService.TurnAllLightsOffInGroup(id);
+            }
+            else
+            {
+                _groupService.TurnAllLightsOnInGroup(id);
+            }
+
+            Groups[GetListId(id)].AllOnState = !Groups[GetListId(id)].AllOnState;
+        }
+
+        private int GetListId(int id)
+        {
+            for (int i = 0; i < Groups.Count; i++)
+            {
+                if (Groups[i].Id == id)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
     }
 }
