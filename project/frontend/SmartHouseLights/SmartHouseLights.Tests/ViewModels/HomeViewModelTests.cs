416fdab380db7a835a3bea8069c228eea394e74f
Sven Wynants <sven.wynants@hotmail.com>
2020-12-31 16:11:28 +0100

    tests for homeviewmodel written

---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/HomeViewModelTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using NUnit.Framework;
+using SmartHouseLights.Models;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Tests.Builders;
+using SmartHouseLights.ViewModels;
+
+namespace SmartHouseLights.Tests.ViewModels
+{
+    [TestFixture]
+    public class HomeViewModelTests
+    {
+        private HomeViewModel _homeViewModel;
+        private Mock<IAuthenticationService> _authServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _authServiceMock = new Mock<IAuthenticationService>();
+            _homeViewModel = new HomeViewModel(_authServiceMock.Object);
+        }
+
+        [Test]
+        public void CreatingHomeViewModelShouldSetCurrentUserInTitle()
+        {
+            User user = new UserBuilder().WithAdminUser().Build();
+            _authServiceMock.Setup(a => a.GetUser())
+                .Returns(() => user);
+            _homeViewModel = new HomeViewModel(_authServiceMock.Object);
+
+            Assert.That(_homeViewModel.User, Is.EqualTo(user));
+            Assert.That(_homeViewModel.Title, Is.EqualTo("Welcome " + user.Name));
+        }
+    }
+}
