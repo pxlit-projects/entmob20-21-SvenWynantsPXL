c392be281255936e9218c653ccd55f9692a858a5
Sven Wynants <sven.wynants@hotmail.com>
2021-01-04 18:27:48 +0100

    Unnecessary functions removed for lightdetails + optimized binding

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/LightDetailsViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/LightDetailsViewModel.cs
@@ -12,13 +12,12 @@ namespace SmartHouseLights.ViewModels
         private Command _flipSwitchCommand;
 
         public Command FlipSwitchCommand => _flipSwitchCommand ??
-                                            (_flipSwitchCommand = new Command(OnFlipSwitch, OnCanExecuteFlipSwitch));
+                                            (_flipSwitchCommand = new Command(OnFlipSwitch));
 
         private Command _changeBrightnessCommand;
 
         public Command OnDragCompletedCommand => _changeBrightnessCommand ??
-                                                 (_changeBrightnessCommand = new Command(OnDragCompleted,
-                                                     OnCanChangeBrightness));
+                                                 (_changeBrightnessCommand = new Command(OnDragCompleted));
         private Light _light;
         public Light Light
         {
@@ -35,7 +34,9 @@ namespace SmartHouseLights.ViewModels
         {
             _lightService = lightService;
             MessagingCenter.Instance.Subscribe<LightListViewModel, Light>(this, MessageConstants.LightSelected,
-                (sender, light) => { Light = light; RefreshCanExecutes();
+                (sender, light) => 
+                {
+                    Light = light;
                     Title = Light.Name;
                 });
         }
@@ -43,38 +44,11 @@ namespace SmartHouseLights.ViewModels
         private void OnFlipSwitch()
         {
             Light = _lightService.FlipSwitch(_light.Id);
-            RefreshCanExecutes();
         }
 
         private void OnDragCompleted()
         {
             Light = _lightService.UpdateLight(Light);
-        }
-
-        private bool OnCanExecuteFlipSwitch()
-        {
-            if (Light != null)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool OnCanChangeBrightness()
-        {
-            if (Light != null && Light.OnState)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private void RefreshCanExecutes()
-        {
-            FlipSwitchCommand.ChangeCanExecute();
-            OnDragCompletedCommand.ChangeCanExecute();
         }
     }
 }
