889ddf4661630d4ea24ab97c11b3db9c4b3b257e
Sven Wynants <sven.wynants@hotmail.com>
2021-01-04 23:32:02 +0100

    LightBuilder adjusted so i can give id myself

--- project/frontend/SmartHouseLights/SmartHouseLights.Tests/Builders/LightBuilder.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/Builders/LightBuilder.cs
@@ -6,18 +6,22 @@ namespace SmartHouseLights.Tests.Builders
     public class LightBuilder
     {
         private readonly Light _light;
-        private static int _id = 1;
+
         public LightBuilder()
         {
             _light = new Light();
-            _light.Id = _id;
-            _id += 1;
             _light.Name = Guid.NewGuid().ToString();
         }
 
         public LightBuilder WithDummy()
         {
             _light.Manufacturer = Manufacturer.DUMMY;
+            return this;
+        }
+
+        public LightBuilder WithId(int id)
+        {
+            _light.Id = id;
             return this;
         }
 
