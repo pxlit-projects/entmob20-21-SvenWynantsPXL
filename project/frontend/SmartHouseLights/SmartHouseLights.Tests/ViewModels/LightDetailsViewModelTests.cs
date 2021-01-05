3c4bf12d8bff98de58641f34b02ca5a6717e62ac
Sven Wynants <sven.wynants@hotmail.com>
2021-01-05 11:26:46 +0100

    LightDetailsViewModelTests created + setup

---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/LightDetailsViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.ViewModels;
+
+namespace SmartHouseLights.Tests.ViewModels
+{
+    [TestFixture]
+    public class LightDetailsViewModelTests
+    {
+        private Mock<ILightService> _lightServiceMock;
+        private LightDetailsViewModel _model;
+
+        [SetUp]
+        public void Setup()
+        {
+            _lightServiceMock = new Mock<ILightService>();
+            _model = new LightDetailsViewModel(_lightServiceMock.Object);
+        }
+    }
+}
