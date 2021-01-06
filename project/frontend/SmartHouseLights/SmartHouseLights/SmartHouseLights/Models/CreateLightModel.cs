fb21d7994dab6beb3ce64db6ba0d1e56b9c3601c
Sven Wynants <sven.wynants@hotmail.com>
2021-01-06 23:26:30 +0100

    OnPropertyChanged added to CreateLightModel

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Models/CreateLightModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Models/CreateLightModel.cs
@@ -1,10 +1,61 @@
-﻿namespace SmartHouseLights.Models
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SmartHouseLights.Annotations;
+
+namespace SmartHouseLights.Models
 {
-    public class CreateLightModel
+    public class CreateLightModel : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public Manufacturer LightManufacturer { get; set; }
-        public int LightGroupId { get; set; }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Manufacturer _manufacturer;
+        public Manufacturer LightManufacturer
+        {
+            get => _manufacturer;
+            set
+            {
+                _manufacturer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _groupId;
+        public int LightGroupId
+        {
+            get => _groupId;
+            set
+            {
+                _groupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
