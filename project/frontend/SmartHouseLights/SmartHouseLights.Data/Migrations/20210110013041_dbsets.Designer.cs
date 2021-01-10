ad5cb73e666e3a0eeb0012001c5c8f9bc577841e
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 03:06:33 +0100

    migrations

--- project/frontend/SmartHouseLights/SmartHouseLights.Data/Migrations/20210110013041_dbsets.Designer.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights.Data/Migrations/20210110013041_dbsets.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartHouseLights.Data;
 
 namespace SmartHouseLights.Data.Migrations
 {
     [DbContext(typeof(SmartHouseContext))]
-    partial class SmartHouseContextModelSnapshot : ModelSnapshot
+    [Migration("20210110013041_dbsets")]
+    partial class dbsets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Data/Migrations/20210110013041_dbsets.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartHouseLights.Data.Migrations
+{
+    public partial class dbsets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLightStatistics_Light_LightId",
+                table: "UserLightStatistics");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLightStatistics_User_UserId",
+                table: "UserLightStatistics");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Light",
+                table: "Light");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Light",
+                newName: "Lights");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Lights",
+                table: "Lights",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLightStatistics_Lights_LightId",
+                table: "UserLightStatistics",
+                column: "LightId",
+                principalTable: "Lights",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLightStatistics_Users_UserId",
+                table: "UserLightStatistics",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLightStatistics_Lights_LightId",
+                table: "UserLightStatistics");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserLightStatistics_Users_UserId",
+                table: "UserLightStatistics");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Lights",
+                table: "Lights");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameTable(
+                name: "Lights",
+                newName: "Light");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Light",
+                table: "Light",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLightStatistics_Light_LightId",
+                table: "UserLightStatistics",
+                column: "LightId",
+                principalTable: "Light",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserLightStatistics_User_UserId",
+                table: "UserLightStatistics",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
