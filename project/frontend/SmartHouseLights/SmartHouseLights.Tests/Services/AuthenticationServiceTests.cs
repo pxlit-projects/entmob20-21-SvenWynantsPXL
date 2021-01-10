95d31d28897ff972b7e6a8a0feed7ec8ea3534c2
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 02:08:21 +0100

    Tests for connectionFactory and authenticationService written

---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/Services/AuthenticationServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using NUnit.Framework;
+using SmartHouseLights.Domain.Models;
+using SmartHouseLights.Services;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Tests.Builders;
+
+namespace SmartHouseLights.Tests.Services
+{
+    [TestFixture]
+    public class AuthenticationServiceTests
+    {
+        private IAuthenticationService _authService;
+        private Mock<IConnectionFactory> _connFactoryMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connFactoryMock = new Mock<IConnectionFactory>();
+            _connFactoryMock.Setup(c => c.GetHttpClient())
+                .Returns(() => new ClientBuilder().Build());
+
+            _authService = new AuthenticationService(_connFactoryMock.Object);
+        }
+
+        [Test]
+        public void LoginShouldReturnNullIfNameOrPasswordIsIncorrect()
+        {
+            User user = _authService.Login("sven", "incorrect").Result;
+
+            Assert.That(user, Is.Null);
+
+            user = _authService.Login("incorrect", "pxl").Result;
+
+            Assert.That(user, Is.Null);
+        }
+
+        [Test]
+        public void LoginShouldReturnUserIfNameAndPasswordAreCorrect()
+        {
+            User user = _authService.Login("sven", "pxl").Result;
+
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.Name, Is.EqualTo("sven"));
+        }
+
+        [Test]
+        public void GetUserShouldReturnUserAfterLogin()
+        {
+            Assert.That(_authService.GetUser(), Is.Null);
+
+            User user = _authService.Login("sven", "pxl").Result;
+
+            User getUser = _authService.GetUser();
+
+            Assert.That(getUser, Is.Not.Null);
+            Assert.That(user.Name, Is.EqualTo(getUser.Name));
+        }
+    }
+}
---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/Services/ConnectionFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using SmartHouseLights.Services;
+using SmartHouseLights.Services.Interfaces;
+
+namespace SmartHouseLights.Tests.Services
+{
+    [TestFixture]
+    public class ConnectionFactoryTests
+    {
+        private IConnectionFactory _connectionFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connectionFactory = new ConnectionFactory();
+        }
+
+        [Test]
+        public void GetHttpClientShouldCreateNewHttpClientWithNoHeaderIfAuthHeaderIsEmpty()
+        {
+            _connectionFactory.RemoveHeader();
+            var client = _connectionFactory.GetHttpClient();
+
+            Assert.That(client, Is.Not.Null);
+            Assert.That(client.DefaultRequestHeaders, Is.Empty);
+        }
+
+        [Test]
+        public void GetHttpClientShouldAddHeaderIfSet()
+        {
+            string header = "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("sven:pxl"));
+            _connectionFactory.SetAuthenticationHeader(header);
+
+            var client = _connectionFactory.GetHttpClient();
+
+            Assert.That(client, Is.Not.Null);
+            Assert.That(client.DefaultRequestHeaders, Is.Not.Empty);
+        }
+
+        [Test]
+        public void RemoveHeaderShouldRemoveCredentials()
+        {
+            string header = "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("sven:pxl"));
+            _connectionFactory.SetAuthenticationHeader(header);
+            
+            var clientWithHeader = _connectionFactory.GetHttpClient();
+            
+            _connectionFactory.RemoveHeader();
+
+            var clientWithoutHeader = _connectionFactory.GetHttpClient();
+
+            Assert.That(clientWithHeader.DefaultRequestHeaders, Is.Not.Empty);
+            Assert.That(clientWithoutHeader.DefaultRequestHeaders, Is.Empty);
+        }
+    }
+}
