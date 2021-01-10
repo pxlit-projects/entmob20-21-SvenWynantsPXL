e2143ad0833d4b3c7d582ea5c3c1ca92d97b6ee9
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 02:09:57 +0100

    tests for AddGroupViewModel and AddLightGroupModel written

---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/AddGroupViewModelTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using NUnit.Framework;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Tests.Builders;
+using SmartHouseLights.ViewModels;
+using SmartHouseLights.Views;
+
+namespace SmartHouseLights.Tests.ViewModels
+{
+    [TestFixture]
+    public class AddGroupViewModelTests
+    {
+        private AddGroupViewModel _model;
+        private Mock<INavigationService> _navServiceMock;
+        private Mock<IGroupService> _groupServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _navServiceMock = new Mock<INavigationService>();
+            _groupServiceMock = new Mock<IGroupService>();
+
+            _model = new AddGroupViewModel(_navServiceMock.Object, _groupServiceMock.Object);
+        }
+
+        [Test]
+        public void OnSaveGroupShouldSetErrorMessageIfNameIsNothing()
+        {
+            _model.SaveGroupCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo("You need to fill in Name"));
+            _groupServiceMock.Verify(g => g.AddGroup(_model.GroupModel), Times.Never);
+        }
+
+        [Test]
+        public void OnSaveGroupShouldSetErrorMessageIfAddingGroupFailed()
+        {
+            _model.GroupModel.Name = "Testgroup";
+            _groupServiceMock.Setup(g => g.AddGroup(_model.GroupModel))
+                .Returns(() => null);
+
+            _model.SaveGroupCommand.Execute(null);
+
+            _groupServiceMock.Verify(g => g.AddGroup(_model.GroupModel), Times.Once);
+            Assert.That(_model.ErrorMessage, Is.EqualTo("Something went wrong adding the group"));
+        }
+
+        [Test]
+        public void OnSaveGroupShouldNavigateToListViewOnSuccess()
+        {
+            _model.GroupModel.Name = "Testgroup";
+            _model.ErrorMessage = "Error message";
+            _groupServiceMock.Setup(g => g.AddGroup(_model.GroupModel))
+                .Returns(() => new GroupBuilder().WithId(1).Build());
+
+            _model.SaveGroupCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo(""));
+            _groupServiceMock.Verify(g => g.AddGroup(_model.GroupModel), Times.Once);
+            _navServiceMock.Verify(n => n.NavigateToAsync($"//{nameof(GroupListView)}"));
+        }
+    }
+}
---
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/AddLightViewModelTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SmartHouseLights.Domain.Models;
+using SmartHouseLights.Models;
+using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Tests.Builders;
+using SmartHouseLights.ViewModels;
+using SmartHouseLights.Views;
+
+namespace SmartHouseLights.Tests.ViewModels
+{
+    [TestFixture]
+    public class AddLightViewModelTests
+    {
+        private AddLightViewModel _model;
+        private Mock<INavigationService> _navServiceMock;
+        private Mock<ILightService> _lightServiceMock;
+        private Mock<IGroupService> _groupServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _navServiceMock = new Mock<INavigationService>();
+            _lightServiceMock = new Mock<ILightService>();
+            _groupServiceMock = new Mock<IGroupService>();
+            _groupServiceMock.Setup(g => g.GetAllGroups())
+                .Returns(() =>
+                {
+                    List<LightGroup> groups = new List<LightGroup>
+                    {
+                        new GroupBuilder().WithId(1).Build(),
+                        new GroupBuilder().WithId(2).Build()
+                    };
+                    return groups;
+                });
+
+            _model = new AddLightViewModel(_navServiceMock.Object, _lightServiceMock.Object, _groupServiceMock.Object);
+        }
+
+        [Test]
+        public void OnSaveLightShouldSetErrorMessageWhenFail()
+        {
+            string errMessage = "All fields need to be entered.";
+            
+            _model.SaveLightCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo(errMessage));
+
+            _model.LightModel.Name = "test";
+
+            _model.SaveLightCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo(errMessage));
+
+            _model.LightModel.Name = "";
+            _model.LightModel.Type = "Test";
+
+            _model.SaveLightCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo(errMessage));
+        }
+
+        [Test]
+        public void OnSaveLightShouldClearErrorMessageOnSuccess()
+        {
+            _model.ErrorMessage = "All fields need to be entered.";
+
+            _model.LightModel.Name = "Test";
+            _model.LightModel.Type = "Test";
+
+            _model.SaveLightCommand.Execute(null);
+
+            Assert.That(_model.ErrorMessage, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void OnSaveLightShouldNavigateToListWhenSuccess()
+        {
+            _model.LightModel.Name = "Test";
+            _model.LightModel.Type = "Test";
+            _lightServiceMock.Setup(l => l.AddLight(_model.LightModel))
+                .Returns(() =>
+                {
+                    Light light = new LightBuilder().WithId(1).WithDummy().Build();
+                    return light;
+                });
+
+            _model.SaveLightCommand.Execute(null);
+            
+            _navServiceMock.Verify(n => n.NavigateToAsync($"//{nameof(LightListView)}"), Times.Once);
+            _lightServiceMock.Verify(l => l.AddLight(_model.LightModel), Times.Once);
+        }
+
+        [Test]
+        public void OnSaveLightShouldSetGroupIdOfSelectedGroup()
+        {
+            _model.LightModel.Name = "Test";
+            _model.LightModel.Type = "Test";
+            _model.CurrentGroup = new GroupBuilder().WithId(1).Build();
+
+            _lightServiceMock.Setup(l => l.AddLight(_model.LightModel))
+                .Returns(() =>
+                {
+                    Light light = new LightBuilder().WithId(1).WithDummy().Build();
+                    return light;
+                });
+
+            _model.SaveLightCommand.Execute(null);
+
+            Assert.That(_model.LightModel.LightGroupId, Is.EqualTo(1));
+        }
+    }
+}
