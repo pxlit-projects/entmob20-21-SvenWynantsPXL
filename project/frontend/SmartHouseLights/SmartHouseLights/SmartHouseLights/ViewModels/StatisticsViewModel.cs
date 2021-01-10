ebbfd62b1b7411e42ec8c606e872d147c51e4337
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 03:10:48 +0100

    private fields removed

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/StatisticsViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/StatisticsViewModel.cs
@@ -7,19 +7,15 @@ namespace SmartHouseLights.ViewModels
 {
     public class StatisticsViewModel : ViewModelBase
     {
-        private readonly IAuthenticationService _authService;
-        private readonly IStatisticsService _statisticsService;
-        private User _user;
         public List<UserLightStatistic> Statistics { get; set; }
+
         public StatisticsViewModel(IAuthenticationService authService, IStatisticsService statisticsService)
         {
-            _authService = authService;
-            _statisticsService = statisticsService;
-            _user = authService.GetUser();
+            var user = authService.GetUser();
 
-            Title = $"Statistics for {_user.Name}";
+            Title = $"Statistics for {user.Name}";
 
-            Statistics = _statisticsService.GetAllStatisticsForUserWithId(_user.Id);
+            Statistics = statisticsService.GetAllStatisticsForUserWithId(user.Id);
         }
     }
 }
