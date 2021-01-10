e5d26bb1c7190814544a8075ff522c38c9913488
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 03:58:43 +0100

    show statistics in view + refresh added

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/StatisticsViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/StatisticsViewModel.cs
@@ -2,20 +2,41 @@
 using SmartHouseLights.Data.Services.Interfaces;
 using SmartHouseLights.Domain.Models;
 using SmartHouseLights.Services.Interfaces;
+using Xamarin.Forms;
 
 namespace SmartHouseLights.ViewModels
 {
     public class StatisticsViewModel : ViewModelBase
     {
-        public List<UserLightStatistic> Statistics { get; set; }
+        private readonly IStatisticsService _statisticsService;
+        private readonly User _user;
+
+        private List<UserLightStatistic> _statistics;
+        public List<UserLightStatistic> Statistics
+        {
+            get => _statistics;
+            set
+            {
+                _statistics = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command RefreshStatsCommand => new Command(OnRefreshStats);
 
         public StatisticsViewModel(IAuthenticationService authService, IStatisticsService statisticsService)
         {
-            var user = authService.GetUser();
+            _statisticsService = statisticsService;
+            _user = authService.GetUser();
 
-            Title = $"Statistics for {user.Name}";
+            Title = $"Statistics for {_user.Name}";
+            
+            Statistics = statisticsService.GetAllStatisticsForUserWithId(_user.Id);
+        }
 
-            Statistics = statisticsService.GetAllStatisticsForUserWithId(user.Id);
+        private void OnRefreshStats()
+        {
+            Statistics = _statisticsService.GetAllStatisticsForUserWithId(_user.Id);
         }
     }
 }
