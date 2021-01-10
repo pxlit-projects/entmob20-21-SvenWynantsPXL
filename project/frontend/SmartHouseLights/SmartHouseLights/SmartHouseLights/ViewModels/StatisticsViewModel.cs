cc881397ef316a0b3749f12fc2470dcc73ecdef4
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 03:07:09 +0100

    StatisticsView added + viewmodel

---
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SmartHouseLights.Data.Services.Interfaces;
+using SmartHouseLights.Domain.Models;
+using SmartHouseLights.Services.Interfaces;
+
+namespace SmartHouseLights.ViewModels
+{
+    public class StatisticsViewModel : ViewModelBase
+    {
+        private readonly IAuthenticationService _authService;
+        private readonly IStatisticsService _statisticsService;
+        private User _user;
+        public List<UserLightStatistic> Statistics { get; set; }
+        public StatisticsViewModel(IAuthenticationService authService, IStatisticsService statisticsService)
+        {
+            _authService = authService;
+            _statisticsService = statisticsService;
+            _user = authService.GetUser();
+
+            Title = $"Statistics for {_user.Name}";
+        }
+    }
+}
