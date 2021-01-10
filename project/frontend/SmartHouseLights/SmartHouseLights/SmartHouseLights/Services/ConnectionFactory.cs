20ca0bbcd95d82ac183785a093be261feee14877
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 02:08:42 +0100

    removeHeader added to connection factory for logout

--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Services/ConnectionFactory.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/Services/ConnectionFactory.cs
@@ -28,5 +28,10 @@ namespace SmartHouseLights.Services
         {
             _authHeader = authHeader;
         }
+
+        public void RemoveHeader()
+        {
+            _authHeader = "";
+        }
     }
 }
