677782d4012941351cbae5dda69ccd9f9bb34023
Sven Wynants <sven.wynants@hotmail.com>
2021-01-10 06:16:35 +0100

    Homeview adjusted + tests written for new function

--- project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/HomeViewModelTests.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights.Tests/ViewModels/HomeViewModelTests.cs
@@ -4,6 +4,7 @@ using SmartHouseLights.Domain.Models;
 using SmartHouseLights.Services.Interfaces;
 using SmartHouseLights.Tests.Builders;
 using SmartHouseLights.ViewModels;
+using SmartHouseLights.Views;
 
 namespace SmartHouseLights.Tests.ViewModels
 {
@@ -12,10 +13,12 @@ namespace SmartHouseLights.Tests.ViewModels
     {
         private HomeViewModel _homeViewModel;
         private Mock<IAuthenticationService> _authServiceMock;
+        private Mock<INavigationService> _navServiceMock;
 
         [SetUp]
         public void Setup()
         {
+            _navServiceMock = new Mock<INavigationService>();
             _authServiceMock = new Mock<IAuthenticationService>();
         }
 
@@ -25,10 +28,22 @@ namespace SmartHouseLights.Tests.ViewModels
             User user = new UserBuilder().WithAdminUser().Build();
             _authServiceMock.Setup(a => a.GetUser())
                 .Returns(() => user);
-            _homeViewModel = new HomeViewModel(_authServiceMock.Object);
+            _homeViewModel = new HomeViewModel(_authServiceMock.Object, _navServiceMock.Object);
 
             Assert.That(_homeViewModel.User, Is.EqualTo(user));
             Assert.That(_homeViewModel.Title, Is.EqualTo("Welcome " + user.Name));
+        }
+
+        [Test]
+        public void GoToStatisticPageShouldNavigate()
+        {
+            User user = new UserBuilder().WithAdminUser().Build();
+            _authServiceMock.Setup(a => a.GetUser())
+                .Returns(() => user);
+            _homeViewModel = new HomeViewModel(_authServiceMock.Object, _navServiceMock.Object);
+            _homeViewModel.GoToStatisticsCommand.Execute(null);
+
+            _navServiceMock.Verify(n => n.NavigateToAsync($"//{nameof(StatisticsView)}"), Times.Once);
         }
     }
 }
--- project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/HomeViewModel.cs
+++ project/frontend/SmartHouseLights/SmartHouseLights/SmartHouseLights/ViewModels/HomeViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using SmartHouseLights.Domain.Models;
 using SmartHouseLights.Services.Interfaces;
+using SmartHouseLights.Views;
+using Xamarin.Forms;
 
 namespace SmartHouseLights.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        private readonly INavigationService _navService;
+        public Command GoToStatisticsCommand => new Command(OnGoToStats);
         public User User { get; set; }
-        public HomeViewModel(IAuthenticationService authenticationService)
+        public HomeViewModel(IAuthenticationService authenticationService, INavigationService navService)
         {
+            _navService = navService;
             User = authenticationService.GetUser();
             Title = "Welcome " + User.Name;
+        }
+
+        private void OnGoToStats()
+        {
+            _navService.NavigateToAsync($"//{nameof(StatisticsView)}");
         }
     }
 }
