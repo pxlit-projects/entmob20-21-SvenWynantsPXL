dce59c4d0f29945b345e9ece8a2e235e315fced8
Sven Wynants <sven.wynants@hotmail.com>
2021-01-14 15:11:29 +0100

    integrationtest created

--- project/smarthome/src/test/java/be/pxl/smarthome/integration/IntegrationTest.java
+++ project/smarthome/src/test/java/be/pxl/smarthome/integration/IntegrationTest.java
@@ -1,4 +1,22 @@
 package be.pxl.smarthome.integration;
 
+import be.pxl.smarthome.service.GroupService;
+import be.pxl.smarthome.service.LightService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
 public class IntegrationTest {
+    @Autowired
+    GroupService groupService;
+    @Autowired
+    LightService lightService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createGroupAndGetAllGroupsTest(){
+        
+    }
 }
