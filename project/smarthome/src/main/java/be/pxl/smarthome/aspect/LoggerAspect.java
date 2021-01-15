8d31fec4acd33441d801fe05d9acb84069f9d01c
Sven Wynants <sven.wynants@hotmail.com>
2021-01-15 12:38:52 +0100

    aspect added for requesting lights

---
+++ project/smarthome/src/main/java/be/pxl/smarthome/aspect/LoggerAspect.java
@@ -0,0 +1,14 @@
+package be.pxl.smarthome.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggerAspect {
+    @Before("execution(* *.getAllLights(..))")
+    public void beforeDeletingLight(){
+        System.out.println("All lights requested");
+    }
+}
